07f8c6ba781de2cd4352b6e43dd26e7fc02d2a1e
artchitector <i@nkuhta.ru>
2023-08-08 17:59:46 +0300

    🪶#20: Добавлен libraries/resizer для картинок

---
+++ libraries/resizer/resizer.go
@@ -0,0 +1,30 @@
+package resizer
+
+import (
+	"github.com/artchitector/artchitect2/model"
+	"github.com/nfnt/resize"
+	"github.com/rs/zerolog/log"
+	"image"
+	"math"
+)
+
+func ResizeImage(img image.Image, size string) image.Image {
+	var height, width uint
+
+	switch size {
+	case model.SizeF:
+		width = model.WidthF
+	case model.SizeM:
+		width = model.WidthM
+	case model.SizeS:
+		width = model.WidthS
+	case model.SizeXS:
+		width = model.WidthXS
+	default:
+		log.Fatal().Msgf("[resizer] wrong size. crash")
+	}
+
+	height = uint(math.Round(float64(width) * model.HeightToWidth))
+	img = resize.Resize(width, height, img, resize.Lanczos3)
+	return img
+}
